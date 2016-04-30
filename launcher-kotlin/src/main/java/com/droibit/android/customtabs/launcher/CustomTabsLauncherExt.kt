2025fbd1e10b69bfd34dadb38f5651d2481e2586
Shinya Kumagai <roomful.rooms@gmail.com>
2016-04-30 22:26:42 +0900

    Added missing extension function

--- launcher-kotlin/src/main/java/com/droibit/android/customtabs/launcher/CustomTabsLauncherExt.kt
+++ launcher-kotlin/src/main/java/com/droibit/android/customtabs/launcher/CustomTabsLauncherExt.kt
@@ -10,6 +10,17 @@ import android.support.customtabs.CustomTabsIntent
  * @param activity The host activity.
  * @param uri the Uri to be opened.
  * @param fallback a [CustomTabsFallback] to be used if Custom Tabs is not available.
+ */
+fun CustomTabsIntent.launch(activity: Activity, uri: String, fallback: ((Activity, Uri)->Unit)? = null) {
+    CustomTabsLauncher.launch(activity, this, Uri.parse(uri), fallback)
+}
+
+/**
+ * Opens the URL on a Custom Tab if possible. Otherwise fallsback to opening it on a WebView.
+ *
+ * @param activity The host activity.
+ * @param uri the [Uri] to be opened.
+ * @param fallback a [CustomTabsFallback] to be used if Custom Tabs is not available.
 */
 fun CustomTabsIntent.launch(activity: Activity, uri: Uri, fallback: ((Activity, Uri)->Unit)? = null) {
     CustomTabsLauncher.launch(activity, this, uri, fallback)
