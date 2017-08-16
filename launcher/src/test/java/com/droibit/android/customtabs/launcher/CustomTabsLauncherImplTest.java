ed784fd7444cff408b90cbe14100a65e1a3ab17f
Shinya Kumgai <roomful.rooms@gmail.com>
2017-08-16 22:02:18 +0900

    Refactoring unit test

--- launcher/src/test/java/com/droibit/android/customtabs/launcher/CustomTabsLauncherImplTest.java
+++ launcher/src/test/java/com/droibit/android/customtabs/launcher/CustomTabsLauncherImplTest.java
@@ -6,10 +6,12 @@ import android.net.Uri;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
+import java.util.Collections;
 import java.util.List;
 
 import static com.droibit.android.customtabs.launcher.CustomTabsLauncherImpl.PACKAGE_BETA;
@@ -17,45 +19,42 @@ import static com.droibit.android.customtabs.launcher.CustomTabsLauncherImpl.PAC
 import static com.droibit.android.customtabs.launcher.CustomTabsLauncherImpl.PACKAGE_LOCAL;
 import static com.droibit.android.customtabs.launcher.CustomTabsLauncherImpl.PACKAGE_STABLE;
 import static java.util.Arrays.asList;
-import static java.util.Collections.singletonList;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
-import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-/**
- * @author kumagai
- */
 public class CustomTabsLauncherImplTest {
 
     @Rule
     public MockitoRule mockito = MockitoJUnit.rule();
 
+    @Mock
+    private PackageManager pm;
+
+    @Mock
+    private Uri uri;
+
     @Spy
-    CustomTabsLauncherImpl launcher;
+    private CustomTabsLauncherImpl launcher;
 
-    // TODO: CustomTabsIntent is final calss.
     public void launchSuccess() {
-
+        // TODO: CustomTabsIntent is final calss.
     }
 
     @Test
-    public void launchFailed() {
-        final Uri uri = mock(Uri.class);
-
+    public void launch_launchFailed() {
         final Activity activity = mock(Activity.class);
-        doReturn(mock(PackageManager.class))
-                .when(activity)
-                .getPackageManager();
+        when(activity.getPackageManager()).thenReturn(mock(PackageManager.class));
 
         final CustomTabsFallback fallback = mock(CustomTabsFallback.class);
-        doReturn(null)
-                .when(launcher)
-                .packageNameToUse(any(PackageManager.class), any(Uri.class));
+        doReturn(null).when(launcher).packageNameToUse(any(PackageManager.class), any(Uri.class));
 
         //noinspection ConstantConditions
         launcher.launch(activity, null, uri, fallback);
@@ -63,61 +62,45 @@ public class CustomTabsLauncherImplTest {
     }
 
     @Test
-    public void decideStableChrome() {
+    public void packageNameToUse_useChromeAsDefault() throws Exception {
+        doReturn(PACKAGE_LOCAL).when(launcher)
+                .defaultViewHandlerPackage(any(PackageManager.class), any(Uri.class));
         doReturn(true)
                 .when(launcher)
                 .supportedCustomTabs(any(PackageManager.class), anyString());
 
-        final List<String> candidates = singletonList(PACKAGE_STABLE);
-        final String stable = launcher.decidePackage(null, candidates);
-        assertThat(stable, equalTo(PACKAGE_STABLE));
+        final String chrome = launcher.packageNameToUse(pm, uri);
+        assertThat(chrome, equalTo(PACKAGE_LOCAL));
     }
 
     @Test
-    public void decideStableChromeFromMultipleVersions() {
-        doReturn(true)
-                .when(launcher)
-                .supportedCustomTabs(any(PackageManager.class), anyString());
+    public void packageNameToUse_notInstalledChrome() throws Exception {
+        doReturn(null).when(launcher)
+                .defaultViewHandlerPackage(any(PackageManager.class), any(Uri.class));
+        doReturn(Collections.emptyList()).when(launcher)
+                .installedPackages(any(PackageManager.class));
+
+        final String packageName = launcher.packageNameToUse(pm, uri);
+        assertThat(packageName, is(nullValue()));
+    }
+
+    @Test
+    public void packageNameToUse_prioritizeStableChrome() throws Exception {
+        doReturn(null).when(launcher)
+                .defaultViewHandlerPackage(any(PackageManager.class), any(Uri.class));
 
         final List<String> candidates = asList(
                 PACKAGE_LOCAL,
                 PACKAGE_DEV,
                 PACKAGE_BETA,
-                PACKAGE_STABLE);
-        final String stable = launcher.decidePackage(null, candidates);
-        assertThat(stable, equalTo(PACKAGE_STABLE));
-    }
-
-    @Test
-    public void decideBetaChrome() {
-        doReturn(false)
-                .when(launcher)
-                .supportedCustomTabs(any(PackageManager.class), eq(PACKAGE_STABLE));
-        doReturn(true)
-                .when(launcher)
-                .supportedCustomTabs(any(PackageManager.class), eq(PACKAGE_BETA));
-
-        final List<String> candidates = asList(
-                PACKAGE_BETA,
-                PACKAGE_STABLE);
-        final String beta = launcher.decidePackage(null, candidates);
-        assertThat(beta, equalTo(PACKAGE_BETA));
-    }
-
-    @Test
-    public void decideDevChromePackage() {
-        // Chrome Dev the default browser.
-        doReturn(PACKAGE_DEV)
-                .when(launcher)
-                .defaultViewHandlerPackage(any(PackageManager.class), any(Uri.class));
-        doReturn(asList(PACKAGE_STABLE, PACKAGE_BETA,  PACKAGE_DEV))
-                .when(launcher)
+                PACKAGE_STABLE
+        );
+        doReturn(candidates).when(launcher)
                 .installedPackages(any(PackageManager.class));
-        doReturn(true)
-                .when(launcher)
+        doReturn(true).when(launcher)
                 .supportedCustomTabs(any(PackageManager.class), anyString());
 
-        final String dev = launcher.packageNameToUse(any(PackageManager.class), any(Uri.class));
-        assertThat(dev, equalTo(PACKAGE_DEV));
+        final String stable = launcher.decidePackage(pm, candidates);
+        assertThat(stable, equalTo(PACKAGE_STABLE));
     }
 }
