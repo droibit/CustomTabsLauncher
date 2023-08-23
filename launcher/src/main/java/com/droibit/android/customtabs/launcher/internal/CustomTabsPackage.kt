79e9e322d42e3eb35f32d603b07a410d29a46bdd
Shinya Kumagai <roomful.rooms@gmail.com>
2023-08-23 12:01:44 +0900

    Support Android 13

--- launcher/src/main/java/com/droibit/android/customtabs/launcher/internal/CustomTabsPackage.kt
+++ launcher/src/main/java/com/droibit/android/customtabs/launcher/internal/CustomTabsPackage.kt
@@ -4,10 +4,10 @@ import android.content.Context
 import android.content.Intent
 import android.content.Intent.ACTION_VIEW
 import android.content.pm.PackageManager
+import android.content.pm.ResolveInfo
 import android.net.Uri
 import android.os.Build
 import androidx.browser.customtabs.CustomTabsService
-
 
 internal object CustomTabsPackage {
     private const val PACKAGE_CHROME_STABLE = "com.android.chrome"
@@ -15,19 +15,18 @@ internal object CustomTabsPackage {
     private const val PACKAGE_CHROME_DEV = "com.chrome.dev"
     private const val PACKAGE_CHROME_LOCAL = "com.google.android.apps.chrome"
 
-    val CHROME_PACKAGES =
-        listOf(PACKAGE_CHROME_STABLE, PACKAGE_CHROME_BETA, PACKAGE_CHROME_DEV, PACKAGE_CHROME_LOCAL)
+    val CHROME_PACKAGES = listOf(
+        PACKAGE_CHROME_STABLE,
+        PACKAGE_CHROME_BETA,
+        PACKAGE_CHROME_DEV,
+        PACKAGE_CHROME_LOCAL
+    )
 
     fun getNonChromeCustomTabsPackages(context: Context): List<String> {
         val activityIntent = Intent(ACTION_VIEW, Uri.parse("http://"))
             .addCategory(Intent.CATEGORY_BROWSABLE)
-        val flag = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            PackageManager.MATCH_ALL
-        } else {
-            PackageManager.MATCH_DEFAULT_ONLY
-        }
         val pm = context.packageManager
-        return pm.queryIntentActivities(activityIntent, flag)
+        return queryIntentActivities(pm, activityIntent)
             .asSequence()
             .map { it.activityInfo.packageName }
             .filter { it !in CHROME_PACKAGES }
@@ -37,5 +36,21 @@ internal object CustomTabsPackage {
                 pm.resolveService(serviceIntent, 0) != null
             }
             .toList()
+    }
+
+    private fun queryIntentActivities(pm: PackageManager, intent: Intent): List<ResolveInfo> {
+        val flag = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            PackageManager.MATCH_ALL
+        } else {
+            PackageManager.MATCH_DEFAULT_ONLY
+        }
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            pm.queryIntentActivities(
+                intent,
+                PackageManager.ResolveInfoFlags.of(flag.toLong())
+            )
+        } else {
+            pm.queryIntentActivities(intent, flag)
+        }
     }
 }
